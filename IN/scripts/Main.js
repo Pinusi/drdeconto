ec3858dba41b0f8d6215f5e401ac0fa342952362
Cecilia De Conto <cecilia.de.conto@pulselive.com>
2015-04-16 13:30:38 +0100

    set up

---
+++ IN/scripts/Main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @name drdecontoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('drdecontoApp', []);
+  // .config(function ($routeProvider) {
+  //   $routeProvider
+  //     .when('/', {
+  //       templateUrl: 'index.html',
+  //       controller: 'MainCtrl'
+  //     })
+  //     .otherwise({
+  //       redirectTo: '/'
+  //     });
+  // });
