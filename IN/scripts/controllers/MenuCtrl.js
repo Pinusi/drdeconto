3cc944d4d62b6f54546ef0a340d06585cd317b9d
Cecilia De Conto <cecilia.de.conto@pulselive.com>
2015-05-06 11:49:55 +0100

    updates

---
+++ IN/scripts/controllers/MenuCtrl.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @name jstestApp.controller:BasketCtrl
+ * @description
+ * # MenuController
+ * Controller of the jstestApp
+ */
+angular.module('drdecontoApp')
+	.controller('MenuCtrl', ['$scope', '$rootScope', function ( $scope, $rootScope ) 
+	{
+		
+	}
+]);
